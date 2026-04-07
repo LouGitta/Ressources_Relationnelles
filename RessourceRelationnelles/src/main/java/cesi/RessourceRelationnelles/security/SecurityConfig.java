3b98c0e7a3fe5aeca6834a5b2319e1e367d1334e
Guyard Elodie <guyard.elodie@outlook.fr>
2026-04-07 09:34:44 +0200

    WIP security

--- RessourceRelationnelles/src/main/java/cesi/RessourceRelationnelles/security/SecurityConfig.java
+++ RessourceRelationnelles/src/main/java/cesi/RessourceRelationnelles/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
+                        .logoutSuccessUrl("/home")
                 );
 
         return http.build();
